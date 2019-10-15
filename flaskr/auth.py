a5dad02967e53aad91e140d94c2b7eaca5fe8567
erivetna87 <erivetna87@gmail.com>
2019-10-15 14:50:24 -0500

    Minor updates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -46,8 +46,8 @@ def register():
                 'INSERT INTO user (username, password) VALUES (?, ?)',
                 (username, generate_password_hash(password))
             )
-        db.commit()
-        return redirect(url_for('auth.login'))
+            db.commit()
+            return redirect(url_for('auth.login'))
         
         flash(error)
     
