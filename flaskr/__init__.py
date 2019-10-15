a8a1783d3bed315e504838ed293dacf42b571fb7
erivetna87 <erivetna87@gmail.com>
2019-10-15 16:09:05 -0500

    Added Config File

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,6 +35,9 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
+    # from config import Config
+    # app.config['SECRET_KEY'] = 'you-will-never-guess'
+
     return app
 
 
